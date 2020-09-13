091049aa092a7dc97b783295dd8ca6a2157f3bef
ybbaek <byb76@hanmail.net>
2020-09-13 21:06:59 +0900

    update
    var define add

--- Arduino/speedsteer-1/speedsteer-1.ino
+++ Arduino/speedsteer-1/speedsteer-1.ino
@@ -3,15 +3,16 @@
 Servo myServo;
 
 int servoPin = 3;
+int first,second,length;
 
-  String data,temp;
-  unsigned long  steer,steerold,go,speed;
+String data,temp;
+String str1,str2,str3;
+unsigned long  steer,steerold,go,speed;
   
 void setup()
 {
     Serial.begin(115200);
-    //int first,second,length;
-    //String str1,str2,str3;
+
     
     myServo.attach(servoPin, 500,2500);
     myServo.write(35);
@@ -25,13 +26,13 @@ void loop()
  // String str1;
   
   if (data != 0){
-    int first = data.indexOf(",");// 첫 번째 콤마 위치
-    int second = data.indexOf(",",first+1); // 두 번째 콤마 위치
-    int length = data.length(); // 문자열 길이
+    first = data.indexOf(",");// 첫 번째 콤마 위치
+    second = data.indexOf(",",first+1); // 두 번째 콤마 위치
+    length = data.length(); // 문자열 길이
   
-    String str1 = data.substring(0, first); // 첫 번째 토큰 (0, 3)
-    String str2 = data.substring(first+1, second); // 두 번째 토큰 (4, 7)
-    String str3 = data.substring(second+1,length); // 세 번째 토큰(8, 10
+    str1 = data.substring(0, first); // 첫 번째 토큰 (0, 3)
+    str2 = data.substring(first+1, second); // 두 번째 토큰 (4, 7)
+    str3 = data.substring(second+1,length); // 세 번째 토큰(8, 10
 
     speed = str1.toInt();
     steer = str2.toInt();
