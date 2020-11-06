19a755d6ea58453fd6498393f8fb3d48b74d700c
ybbaek <byb76@hanmail.net>
2020-11-07 00:21:53 +0900

    joystick update
    xbox, SRC B-type add/update

---
+++ Arduino/SRC-A/SRC-A.ino
@@ -0,0 +1,55 @@
+#include<Servo.h>
+
+Servo myServo;
+
+int servoPin = 3;
+int first,second,length;
+
+String data,temp;
+String str1,str2,str3;
+unsigned long  steer,steerold,go,speed;
+  
+void setup()
+{
+    Serial.begin(115200);
+
+    
+    myServo.attach(servoPin, 500,2500);
+    myServo.write(35);
+    
+}
+ 
+void loop()
+{
+  data = Serial.readStringUntil('t');
+
+ // String str1;
+  
+  if (data != 0){
+    first = data.indexOf(",");// 첫 번째 콤마 위치
+    second = data.indexOf(",",first+1); // 두 번째 콤마 위치
+    length = data.length(); // 문자열 길이
+  
+    str1 = data.substring(0, first); // 첫 번째 토큰 (0, 3)
+    str2 = data.substring(first+1, second); // 두 번째 토큰 (4, 7)
+    str3 = data.substring(second+1,length); // 세 번째 토큰(8, 10
+
+    speed = str1.toInt();
+    steer = str2.toInt();
+
+ //   if(steer =! steerold){
+      myServo.write(steer);
+ //   }
+    //analogWrite(3, speed);
+
+    steerold = steer;
+
+    Serial.println("sp"+str1+","+"st"+str2);
+    
+    }
+    else{
+      digitalWrite(13, LOW);
+    //  analogWrite(3, 0);
+    }
+   // Serial.print(speed);
+}
