47f9cf159084196d9371ca621ecc36ceffaa922a
ybbaek <byb76@hanmail.net>
2021-05-30 09:20:40 +0900

    ASV code udpate
    vi test code & arduino code

--- Arduino/UAV-A1/UAV-A1.ino
+++ Arduino/UAV-A1/UAV-A1.ino
@@ -9,8 +9,8 @@ String data_g; //global serial receive data from latte
 char ledchar_g[] = "aaa"; //global leddot matrix character
 
 
-#define MIN_PULSE_LENGTH 1050 // Minimum pulse length in µs
-#define MAX_PULSE_LENGTH 1200 // Maximum pulse length in µs
+#define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
+#define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
 
 textEffect_t texteffect[] =
 {
@@ -56,7 +56,9 @@ MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 Servo myServo;
 
-int servoPin = 5;
+int servoPin = 5; // SRC steer
+int motLPin = 6;     // ASV left servo
+int motRPin = 9;     // ASV right servo
 
 class Flasher
 {
@@ -109,11 +111,11 @@ class Flasher
   }
 };
 
-  int dir=4;
+  int dir=4; // DC motor direction
   int pwm;
   int light=2;
   String data,temp;
-  unsigned long  steer,go,speed;
+  unsigned long  steer,go,speed_l, speed_r;
   
 //Flasher led1(8, 2000, 1000);
 
@@ -122,7 +124,7 @@ char ledchar[] = "RLmodel Unmanned Surface Vessel";
 
 Flasher led1(8, 2000, 1000);
 
-Servo motA, motB, motC, motD;
+Servo motL, motR;
 //char data;
 
 void setup()
@@ -138,9 +140,10 @@ void setup()
   //int first,second,length;
   //String str1,str2,str3;
     
-  myServo.attach(servoPin, 500,2500);
+  myServo.attach(servoPin, 500,2500); // steer servo
   myServo.write(35);
-  motC.attach(6, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  motL.attach(motLPin, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  motR.attach(motRPin, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
     
   pinMode(dir, OUTPUT); // 방향
   pinMode(light, OUTPUT); // 라이트
@@ -174,22 +177,26 @@ void loop()
     int third = data.indexOf(",",second+1); // 세 번째 콤마 위치
     int fourth = data.indexOf(",",third+1); // 네 번째 콤마 위치
     int fifth = data.indexOf(",",fourth+1); //
+    int sixth = data.indexOf(",",fifth+1); //
     int length = data.length(); // 문자열 길이
     
-    String str1 = data.substring(0, first); // 속도
+    String str1 = data.substring(0, first); // 속도 L
     String str2 = data.substring(first+1, second); // 조향
     String str3 = data.substring(second+1,third); // 전후진
     String str4 = data.substring(third+1,fourth); // 라이트
     String str5 = data.substring(fourth+1,fifth); // info msg
+    String str6 = data.substring(fifth+1, sixth); // 속도 R 
     
     //ledmsg = data.substring(fourth+1,length); // 메세지
     
-    speed = str1.toInt()* 10;
+    speed_l = str1.toInt()* 10;
+    speed_r = str6.toInt()* 10;
     
     steer = str2.toInt();
 
   //analogWrite(6, speed); // SRC 제어
-    motC.writeMicroseconds(speed);
+    motL.writeMicroseconds(speed_l);
+    motR.writeMicroseconds(speed_r);
 
     // 전,후진 ---------------------------------
     if(str3 == "b") // 후진
@@ -221,13 +228,14 @@ void loop()
     led1.Update();
     myServo.write(steer);
 
-    Serial.println("sp="+str1+","+"st="+str2+","+"dir="+str3+","+"light="+str4);
+    Serial.println("spl="+str1+","+"st="+str2+","+"dir="+str3+","+"light="+str4+","+"spr="+str6+",");
     //Serial.println(str4);
     }
     else{ // 통신끊힘 ->null 데이타
       digitalWrite(dir, LOW);
       //analogWrite(6, 0);      //통신오류 시 차량정지
-      motC.writeMicroseconds(1000);
+      motL.writeMicroseconds(1000);
+      motR.writeMicroseconds(1000);
     }
 
 }
